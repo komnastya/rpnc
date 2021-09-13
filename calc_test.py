babf2507a02e19885c63403608f52b9f3e3ce594
Nastassia Kamiakova <nastassia294@gmail.com>
2021-09-13 16:30:03 +0300

    Add the test for the calc function

---
+++ calc_test.py
@@ -0,0 +1,32 @@
+import pytest
+
+from calc import calc
+from types import ArgError
+
+
+def test_calc():
+    with pytest.raises(ArgError):
+        calc([])
+    with pytest.raises(ArgError):
+        calc([1])
+    with pytest.raises(ArgError):
+        calc(['+'])
+    with pytest.raises(ArgError):
+        calc([1, '+'])
+    with pytest.raises(ArgError):
+        calc([1, 2, 3, '+'])
+    with pytest.raises(ArgError):
+        calc([1, 2, '+', '+'])
+
+    assert calc([3, 2, "+"]) == 5
+    assert calc([3, 2, "-"]) == 1
+    assert calc([3, 2, "*"]) == 6
+    assert calc([3, 2, "/"]) == 1.5
+
+    assert calc([3, 4, "*", 2, '/']) == 6
+    assert calc([3, 11, 5, "+", "-"]) == -13
+    assert calc([5, 9, 2, "*", "+"]) == 23
+    assert calc([1, 2, "*", 3, 4, "*", "+"]) == 14
+
+    with pytest.raises(ArgError):
+        assert calc([3, 4, "x", 2, ':'])
