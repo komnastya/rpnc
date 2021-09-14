eda36a8acf0c7ba012d4b5786414b2746b7d41ca
Nastassia Kamiakova <nastassia294@gmail.com>
2021-09-14 14:47:24 +0300

    Improve the calc function

--- calc.py
+++ calc.py
@@ -8,20 +8,29 @@ def calc(args: Iterable[Arg]) -> Union[int, float]:
     for arg in args:
         if type(arg) == int or type(arg) == float:
             stack.append(arg)
-        elif arg in ["+", "-", "*", "/"] and len(stack) >= 2:
-            a = stack.pop()
-            b = stack.pop()
-            if arg == "+":
-                c = a + b
-            elif arg == "-":
-                c = b - a
-            elif arg == "*":
-                c = a * b
-            elif arg == "/":
-                c = b / a
-            stack.append(c)
         else:
-            raise ArgError
+            if arg == "+" and len(stack) >= 2:
+                a = stack.pop()
+                b = stack.pop()
+                c = a + b
+                stack.append(c)
+            elif arg == "-" and len(stack) >= 2:
+                a = stack.pop()
+                b = stack.pop()
+                c = b - a
+                stack.append(c)
+            elif arg == "*" and len(stack) >= 2:
+                a = stack.pop()
+                b = stack.pop()
+                c = a * b
+                stack.append(c)
+            elif arg == "/" and len(stack) >= 2:
+                a = stack.pop()
+                b = stack.pop()
+                c = b / a
+                stack.append(c)
+            else:
+                raise ArgError
     if len(stack) != 1:
         raise ArgError
     return stack[0]
