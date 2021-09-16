779f6b3783bee74578b4f5b31b4649f2841fae65
Nastassia Kamiakova <nastassia294@gmail.com>
2021-09-16 13:03:59 +0300

    Improve the main function

--- main.py
+++ main.py
@@ -9,21 +9,17 @@ print("Welcome to the calculator!")
 
 def main():
     while True:
-        input_exp = input(
-            "Please, input the expression in Reversed Polish Notation: "
-        ).strip()
+        input_exp = input("Please, input the expression in Reversed Polish Notation: ")
         if input_exp == "stop":
             print("Good bye!")
             break
-        else:
-            try:
-                parsed_exp = parse_exp(input_exp)
-                print(f"The result of '{print_exp(parsed_exp)}' is {calc(parsed_exp)}")
-            except ParseError as error:
-                print(error)
-                print("Please, enter correct expression")
-            except ArgError as error:
-                print(error)  # TODO
+        try:
+            parsed_exp = parse_exp(input_exp)
+            print(f"The result of '{print_exp(parsed_exp)}' is {calc(parsed_exp)}")
+        except ParseError as error:
+            print(f"Parse error: {error}")
+        except ArgError as error:
+            print(f"Compute error: {error}")
 
 
 if __name__ == "__main__":
