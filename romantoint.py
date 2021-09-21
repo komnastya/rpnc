03b512fcb29e16d9200f13d4282f84b5871d039f
Nastassia Kamiakova <nastassia294@gmail.com>
2021-09-21 16:43:44 +0300

    Improve the roman to int function

--- romantoint.py
+++ romantoint.py
@@ -1,10 +1,9 @@
+chars = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
 def roman_to_int(s: str) -> int:
     number = 0
 
-    chars = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-
     prev_value = 0
-
     for i in range(len(s) - 1, -1, -1):
         cur_value = chars[s[i]]
         if cur_value >= prev_value:
